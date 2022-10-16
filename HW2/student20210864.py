8869aa1c18eacf5f7253148f650ae7ba88b1f8af
20210864 <nndhfs123@naver.com>
2022-10-17 03:24:21 +0900

    check

--- HW2/student20210864.py
+++ HW2/student20210864.py
@@ -9,9 +9,9 @@ ws = wb.active
 ws.title = "Sheet1"
 
 dic = dict()
-max_st = ws.max_row - 1
+max_st = ws.max_row
 
-for i in range(2, max_st + 2):
+for i in range(2, max_st + 1):
     midterm = ws.cell(row = i, column = 3).value
     final = ws.cell(row = i, column = 4).value
     hw = ws.cell(row = i, column = 5).value
@@ -35,16 +35,19 @@ same_dic = dict()
 same_keys = list()
 i_same = 0
 i = 0
-while(st_cnt <= max_st):
-    if result_dict[dict_keys[i]] in score:
-        i_same = score.count(result_dict[dict_keys[i]].value)
-        if i_same <= A_max:
+same_dic['A'] = 0
+same_dic['B'] = 0
+same_dic['C'] = 0
+while(st_cnt < max_st - 1):
+    if result_dict[dict_keys[i]] in score[i+1:]:
+        i_same = score.count(result_dict[dict_keys[i]])
+        if st_cnt + i_same <= A_max:
             for k in range(st_cnt, st_cnt + i_same):
                 A_st.append(dict_keys[k])
                 same_keys.append(dict_keys[k])
             same_dic['A'] = same_keys
             same_dic[score[i]] = i_same
-        elif i_same <= A_max + B_max:
+        elif st_cnt + i_same <= max_st * 0.7:
             for k in range(st_cnt, st_cnt + i_same):
                 B_st.append(dict_keys[k])
                 same_keys.append(dict_keys[k])
@@ -57,21 +60,24 @@ while(st_cnt <= max_st):
             same_dic['C'] = same_keys
             same_dic[score[i]] = i_same
         same_keys.clear()
-        
+
     else:
         if st_cnt <= A_max:
-            A_st.append(dict_keys[i]) 
+            A_st.append(dict_keys[i])
         elif st_cnt <= A_max + B_max:
-            B_st.append(dict_keys[i]) 
+            B_st.append(dict_keys[i])
         else:
             C_st.append(dict_keys[i])
     i = st_cnt
     st_cnt += 1
 st_cnt = 1
 i = 0
+a_list = same_dic['A']
+b_list = same_dic['B']
+c_list = same_dic['C']
 while(st_cnt < len(A_st)):
-    if A_st[i] in same_dic['A']:
-        s_cnt = same_dic[A_st[i]].value
+    if str(A_st[i]) in str(a_list):
+        s_cnt = same_dic[A_st[i]]
         if s_cnt + st_cnt <= len(A_st) * 0.5:
             for a in range(s_cnt):
                 ws.cell(row = A_st[i], column = 8, value = "A+")
@@ -85,13 +91,13 @@ while(st_cnt < len(A_st)):
         else:
             ws.cell(row = A_st[i], column = 8, value = "A0")
     i = st_cnt
-    st_cnt += 1 
-
+    st_cnt += 1
+st_cnt = 1
 i = 0
-while(st_cnt < len(A_st + B_st)):
-    if B_st[i] in same_dic['B']:
+while(st_cnt < len(B_st)):
+    if str(B_st[i]) in str(b_list):
         s_cnt = same_dic[B_st[i]].value
-        if s_cnt + st_cnt <= len(A_max + B_max) * 0.5:
+        if s_cnt + st_cnt <= len(B_st) * 0.5:
             for a in range(s_cnt):
                 ws.cell(row = B_st[i], column = 8, value = "B+")
         else:
@@ -99,18 +105,18 @@ while(st_cnt < len(A_st + B_st)):
                 ws.cell(row = B_st[i], column = 8, value = "B0")
         st_cnt += s_cnt
     else:
-        if st_cnt <= len(A_max + B_max) * 0.5:
+        if st_cnt <= len(B_st) * 0.5:
             ws.cell(row = B_st[i], column = 8, value = "B+")
         else:
             ws.cell(row = B_st[i], column = 8, value = "B0")
     i = st_cnt
     st_cnt += 1
-
+st_cnt = 1
 i = 0
-while(st_cnt < len(A_st + B_st + C_st)):
-    if C_st[i] in same_dic['C']:
+while(st_cnt < len(C_st)):
+    if str(C_st[i]) in str(c_list):
         s_cnt = same_dic[C_st[i]].value
-        if s_cnt + st_cnt <= len(A_max + B_max + C_max) * 0.5:
+        if s_cnt + st_cnt <= len(C_st) * 0.5:
             for a in range(s_cnt):
                 ws.cell(row = C_st[i], column = 8, value = "C+")
         else:
@@ -118,7 +124,7 @@ while(st_cnt < len(A_st + B_st + C_st)):
                 ws.cell(row = C_st[i], column = 8, value = "C0")
         st_cnt += s_cnt
     else:
-        if st_cnt <= len(A_max + B_max + C_max) * 0.5:
+        if st_cnt <= len(C_st) * 0.5:
             ws.cell(row = C_st[i], column = 8, value = "C+")
         else:
             ws.cell(row = C_st[i], column = 8, value = "C0")
