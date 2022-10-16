22b1f6638afb5fb7889bce8f3084dda01e82d2b9
20210864 <nndhfs123@naver.com>
2022-10-17 00:29:14 +0900

    check

--- HW2/student20210864.py
+++ HW2/student20210864.py
@@ -9,86 +9,77 @@ ws = wb.active
 ws.title = "Sheet1"
 
 dic = dict()
-max_st = ws.max_row
+max_st = ws.max_row - 1
+score = list()
 
-for i in range(2, max_st + 1):
+for i in range(2, max_st + 2):
     midterm = ws.cell(row = i, column = 3).value
     final = ws.cell(row = i, column = 4).value
     hw = ws.cell(row = i, column = 5).value
     attendance = ws.cell(row = i, column = 6).value
     result = float(midterm*0.3 + final*0.35 + hw*0.34 + attendance*1)
     dic[i] = result
+    score.append(result)
     ws.cell(row = i, column = 7, value = result)
 
 result_dict = dict(sorted(dic.items(), reverse = True, key = lambda x:x[1]))
 
+A_max = max_st * 0.3
+B_max = max_st * 0.4
+C_max = max_st * 0.3
+st_cnt = 1
+
 dict_keys = result_dict.keys()
 dict_keys = list(dict_keys)
-
-st_count = 1
-same_cnt = 0
-same_isit = 0
-same1 = 0
-same2 = max_st + 1
+print(result_dict)
+print(score[1:])
+A_st = list()
+B_st = list()
+C_st = list()
 
 for i in range(max_st - 1):
-    if i <= max_st - 3:
-        a1 = dict_keys[i]
-        a2 = dict_keys[i+1]
-        while True:
-            if result_dict[a1] == result_dict[a2]: 
-                same2 = i + 1
-                same_cnt += 1
-                if same_cnt == 1:
-                    same1 = i + 1
-            else:
-                break
-        for j in range(same1, same2+1):
-            if j == 15 or j == 30 or j == 50 or j == 70 or j == 85:
-                same_isit = 1
-                st_count = same2
+    if dict_keys[i] in score:
+        i_same = score.count(dic[i].value)
 
-    elif i == max_st - 2:
-       pass 
-
+        if i_same <= A_max:
+            for k in range(st_cnt, st_cnt + i_same):
+                A_st.append(dict_keys[k])
+        elif A_max < i_same <= A_max + B_max:
+            for k in range(st_cnt, st_cnt + i_same):
+                B_st.append(dict_keys[i])
+        else:
+            for k in range(st_cnt, st_cnt + i_same):
+                C_st.append(dict_keys[i])
+        
     else:
-        break
+        if st_cnt <= A_max:
+            A_st.append(dict_keys[i]) 
+        elif A_max < st_cnt <= A_max + B_max:
+            B_st.append(dict_keys[i]) 
+        else:
+            C_st.append(dict_keys[i])
+    st_cnt += 1
+    
+    for j in A_st:
+        for a in range(int(len(A_st) * 0.5)):
+            ws.cell(row = result_dict[j], column = 8, value = "A+")
+        
+        for a in range(int(len(A_st) * 0.5)):
+            ws.cell(row = result_dict[j], column = 8, value = "A0")
+    
+    for j in B_st:
+        for a in range(int(len(B_st) * 0.5)):
+            ws.cell(row = result_dict[j], column = 8, value = "B+")
+        
+        for a in range(int(len(B_st) * 0.5)):
+            ws.cell(row = result_dict[j], column = 8, value = "B0")
 
-    if st_count <= (max_st - 1) * 0.15:
-        if same_isit == 1:
-            for k in range(same1, same2):
-                ws.cell(row = dict_keys[k - 1], column = 8, value = "A+")
-        ws.cell(row = dict_keys[i], column = 8, value = "A+")
-
-    elif (max_st - 1) * 0.15 < st_count <= (max_st - 1) * 0.3:
-        if same_isit == 1:
-            for k in range(same1, same2):
-                ws.cell(row = dict_keys[k - 1], column = 8, value = "A0")
-        ws.cell(row = dict_keys[i], column = 8, value = "A0")
-
-    elif (max_st - 1) * 0.3 < st_count <= (max_st - 1) * 0.5:
-        if same_isit == 1:
-            for k in range(same1, same2):
-                ws.cell(row = dict_keys[k - 1], column = 8, value = "B+")
-        ws.cell(row = dict_keys[i], column = 8, value = "B+")
-
-    elif (max_st - 1) * 0.5 < st_count <= (max_st - 1) * 0.7:
-        if same_isit == 1:
-            for k in range(same1, same2):
-                ws.cell(row = dict_keys[k - 1], column = 8, value = "B0")
-        ws.cell(row = dict_keys[i], column = 8, value = "B0")
-
-    elif (max_st - 1) * 0.7 < st_count <= (max_st - 1) * 0.85:
-        if same_isit == 1:
-            for k in range(same1, same2):
-                ws.cell(row = dict_keys[k - 1], column = 8, value = "C+")
-        ws.cell(row = dict_keys[i], column = 8, value = "C+")
-
-    else:
-        if same_isit == 1:
-            for k in range(same1, same2):
-                ws.cell(row = dict_keys[k - 1], column = 8, value = "C0")
-        ws.cell(row = dict_keys[i], column = 8, value = "C0")
-    st_count += 1
+    
+    for j in C_st:
+        for a in range(int(len(C_st) * 0.5)):
+            ws.cell(row = result_dict[j], column = 8, value = "C+")
+        
+        for a in range(int(len(C_st) * 0.5)):
+            ws.cell(row = result_dict[j], column = 8, value = "C0")
 
 wb.save(filename = dest_filename)
