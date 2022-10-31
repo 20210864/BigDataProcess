e69b8a3af3589ae03749b358717d0f28f4d92b89
20210864 <nndhfs123@naver.com>
2022-10-31 15:23:19 +0900

    commit

--- HW3/UBERStudent20210864.py
+++ HW3/UBERStudent20210864.py
@@ -7,6 +7,7 @@ def uberInfo(file1, file2):
     f.close()
 
     uList = list()
+    uDict = dict()
     rows = ip.split('\n')
     days = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
 
@@ -31,20 +32,48 @@ def uberInfo(file1, file2):
         day = date(year, mon, d).weekday()
         uList.append([f1, days[day], f3, f4])
 
+    region = [i[0] for i in uList]
+    region = set(region)
+
+    for r in region:
+        c = []
+        for u in uList:
+            if r == u[0]:
+                c.append(u[1])
+        c = list(set(c))
+        uDict[r] = c
+
+    result = list()
+
+    for ud in uDict:
+        for uv in uDict[ud]:
+            vehicles = 0
+            trips = 0
+            for i in uList:
+                if ud == i[0] and uv == i[1]:
+                    vehicles += int(i[2])
+                    trips += int(i[3])
+
+            result.append([ud, uv, vehicles, trips])
+
     f = open(file2, "wt")
     cnt = 0
     for row in rows:
-        f.write(uList[cnt][0])
-        f.write(",")
-        f.write(uList[cnt][1])
-        f.write(" ")
-        f.write(uList[cnt][2])
-        f.write(",")
-        f.write(uList[cnt][3])
-        f.write("\n")
+        for frr in result:
+            fc = 0
+            for fr in frr:
+                f.write(str(fr))
+                if fc == 1:
+                    f.write(" ")
+                elif fc == 3:
+                    f.write("\n")
+                else:
+                    f.write(",")
+                fc += 1
         cnt += 1
 
     f.close()
 
 a, b, c = sys.argv
-uberInfo(b, c) 
+uberInfo(b, c)
+
